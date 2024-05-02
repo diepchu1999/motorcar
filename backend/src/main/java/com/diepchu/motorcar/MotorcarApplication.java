d8b0cba139d919262b8dbd4ada53d2fc1e5fcf97
diepchu1999 <chuvandiep2804@gmail.com>
2024-05-02 12:32:29 +0700

    Ticket #ARES-1 Setup source code

---
+++ backend/src/main/java/com/diepchu/motorcar/MotorcarApplication.java
@@ -0,0 +1,13 @@
+package com.diepchu.motorcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorcarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/diepchu/motorcar/MotorcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diepchu.motorcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
